6c44a3ee5c5d297076325af59a9a40265906d9d1
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-08-11 17:46:04 -0400

    iniciando proyecto de peliculas

---
+++ src/test/java/com/jp/project/MovieManagement/MovieManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.project.MovieManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

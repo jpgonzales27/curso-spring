98a3db21c4dc968e772779d22115d67a6987a999
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-08-13 16:22:13 -0400

    creando todos los metodos de userController

--- src/main/java/com/jp/project/MovieManagement/persistence/repository/UserCrudRepository.java
+++ src/main/java/com/jp/project/MovieManagement/persistence/repository/UserCrudRepository.java
@@ -2,6 +2,19 @@ package com.jp.project.MovieManagement.persistence.repository;
 
 import com.jp.project.MovieManagement.persistence.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+
+import java.util.List;
 
 public interface UserCrudRepository extends JpaRepository<User,Long> {
+    List<User> findByNameContaining(String name);
+    User findByUsername(String username);
+
+    /**
+     * Modifying indica a JPA que la consulta modificara la BD
+     * es decir son consultas SQL como DELETE, INSERT o UPDATE
+     * y deben ser ejecutas en el contexto transaccional
+     */
+    @Modifying
+    void deleteByUsername(String username);
 }

cb02d1428fe940ea4b3ba84f12f5ab0280c697ee
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-08-14 19:06:07 -0400

    fixeando movieController para devolver los resultados

--- src/main/java/com/jp/project/MovieManagement/controller/MovieController.java
+++ src/main/java/com/jp/project/MovieManagement/controller/MovieController.java
@@ -4,19 +4,21 @@ import com.jp.project.MovieManagement.persistence.entity.Movie;
 import com.jp.project.MovieManagement.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/movie")
+@RequestMapping("/movies")
 public class MovieController {
 
     @Autowired
     private MovieService movieService;
 
-    @GetMapping
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
     public List<Movie> findAllMovies() {
         return movieService.findAll();
     }

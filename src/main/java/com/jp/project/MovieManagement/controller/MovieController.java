fbf606158481276a7871775ca497f7d37eeddb66
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-08-14 19:18:34 -0400

    Usnado RestController en MovieController

--- src/main/java/com/jp/project/MovieManagement/controller/MovieController.java
+++ src/main/java/com/jp/project/MovieManagement/controller/MovieController.java
@@ -3,14 +3,13 @@ package com.jp.project.MovieManagement.controller;
 import com.jp.project.MovieManagement.persistence.entity.Movie;
 import com.jp.project.MovieManagement.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/movies")
 public class MovieController {
 
@@ -18,7 +17,6 @@ public class MovieController {
     private MovieService movieService;
 
     @RequestMapping(method = RequestMethod.GET)
-    @ResponseBody
     public List<Movie> findAllMovies() {
         return movieService.findAll();
     }

ac87e1939644452ad163710ca812e0c26b704dae
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-08-21 20:18:11 -0400

    Fix InvalidPasswordException

--- src/main/java/com/jp/project/MovieManagement/exception/InvalidPasswordException.java
+++ src/main/java/com/jp/project/MovieManagement/exception/InvalidPasswordException.java
@@ -6,14 +6,13 @@ public class InvalidPasswordException extends RuntimeException {
     private final String passwordRepeated;
     private final String errorMessage;
 
-    public InvalidPasswordException(String password, String errorMessage) {
+    public InvalidPasswordException(String password,  String errorMessage) {
         this.password = password;
         this.passwordRepeated = password;
         this.errorMessage = errorMessage;
     }
 
-    public InvalidPasswordException(String message, String password, String passwordRepeated, String errorMessage) {
-        super(message);
+    public InvalidPasswordException(String password, String passwordRepeated, String errorMessage) {
         this.password = password;
         this.passwordRepeated = passwordRepeated;
         this.errorMessage = errorMessage;
